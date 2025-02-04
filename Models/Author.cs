5a93f98030a5e452eab0b6c093eb8933dce36826
Isa Westling <isawestling.blogg@hotmail.com>
2025-02-04 17:04:13 +0100

    Skapat en modell för författare med properties för id och namn, inkluderande data annotations för validering (required) och namn (display).

---
+++ Models/Author.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations; // Inkluderar dataannotations för att kunna använda required
+
+namespace BookApp.Models;
+
+// Modell för författare
+public class Author
+{
+    public int Id { get; set; } // Primärnyckel
+
+    [Required] // Obligatoriskt
+    [Display(Name = "Författare")] // Visa "Författare" i formuläret
+    public string Name { get; set; } = string.Empty; // Namn på författaren
+
+    // Lista med böcker som tillhör författaren från Book-modellen
+    public List<Book> Books { get; set; } = new List<Book>();
+}
