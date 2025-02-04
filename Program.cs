c7061fe342eb2fd19611c84c34c36fc9d0fc0617
Isa Westling <isawestling.blogg@hotmail.com>
2025-02-04 17:14:34 +0100

    Lagt till databaskoppling med Entity Framework Core och SQLite

--- Program.cs
+++ Program.cs
@@ -1,7 +1,14 @@
+using Microsoft.EntityFrameworkCore; // Inkludera Entity Framework Core
+using BookApp.Data; // Inkludera ApplicationDbContext
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// Skapa en databaskoppling med Entity Framework Core och SQLite
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
