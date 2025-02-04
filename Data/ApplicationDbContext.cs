947d370c5a6e0054da4d626ab041b7fcb379663b
Isa Westling <isawestling.blogg@hotmail.com>
2025-02-04 17:13:56 +0100

    Skapat ApplicationDbContext för att definiera databaskopplingen och DbSet för böcker och författare.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using BookApp.Models; // Inkludera modellerna Book och Author
+
+namespace BookApp.Data;
+
+// Klass för att skapa databaskopplingen
+public class ApplicationDbContext : DbContext
+{
+    // Konstruktor som tar emot inställningar och skickar dem vidare till basklassen
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    // DbSet för att skapa tabeller i databasen
+    public DbSet<Book> Books { get; set; } // Tabell för böcker baserat på modellen Book
+    public DbSet<Author> Authors { get; set; } // Tabell för författare baserat på modellen Author
+}
