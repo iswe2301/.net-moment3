a73265aa23f26aa86a115078b5559decac38d54a
Isa Westling <isawestling.blogg@hotmail.com>
2025-02-05 13:08:52 +0100

    Lagt till anpassade felmeddelanden för obligatoriska fält i Book-modellen, lagt till display för författare

--- Models/Book.cs
+++ Models/Book.cs
@@ -7,18 +7,20 @@ public class Book
 {
     public int Id { get; set; } // Primärnyckel
 
-    [Required] // Obligatoriskt fält
+    [Required(ErrorMessage = "Titel måste anges")] // Titel måste anges
     [Display(Name = "Titel")] // Visa "Titel" i formuläret
     public string Title { get; set; } = string.Empty; // Titel på boken
 
-    [Required] // Obligatoriskt fält
+    [Required(ErrorMessage = "Utgivningsår måste anges")] // Utgivningsår måste anges
     [Display(Name = "Utgivningsår")] // Visa "Utgivningsår" i formuläret
     public int PublicationYear { get; set; } // Utgivningsår
 
     // Främmandenyckel för författare
-    [Required] // Obligatoriskt fält
+    [Required(ErrorMessage = "Författare måste anges")] // Författare måste anges
+    [Display(Name = "Författare")] // Visa "Författare" i formuläret
     public int AuthorId { get; set; }
 
     // Författare som boken tillhör från Author-modellen
+    [Display(Name = "Författare")] // Visa "Författare" i formuläret
     public Author? Author { get; set; }
 }
